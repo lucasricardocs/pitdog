ae3bb9c9539b73dfd779f08365d32fecdc535daf
lucasricardocs <lucasricardo@gmail.com>
2025-04-19 17:54:38 -0300

    pitdog.py

--- pitdog.py
+++ pitdog.py
@@ -294,9 +294,9 @@ if arquivo:
              expander_title = f"**{forma}** (Total: {format_currency(total_pagamento)})"
              with st.expander(expander_title, expanded=False):
                  diferenca = valor_total_calc - total
-cor_diferenca = "red" if diferenca != 0 else "green"
-sinal = "+" if diferenca > 0 else ""
-st.markdown(f"<span style='font-size: large; color: steelblue;'>Valor Calculado: <b style='color: darkblue;'>{format_currency(valor_total_calc)}</b></span> | <span style='font-size: medium; color: grey;'>Diferença: <b style='color: {cor_diferenca};'>{sinal}{format_currency(diferenca)}</b></span>", unsafe_allow_html=True)
+                 cor_diferenca = "red" if diferenca != 0 else "green"
+                 sinal = "+" if diferenca > 0 else ""
+                 st.markdown(f"<span style='font-size: large; color: steelblue;'>Valor Calculado: <b style='color: darkblue;'>{format_currency(valor_total_calc)}</b></span> | <span style='font-size: medium; color: grey;'>Diferença: <b style='color: {cor_diferenca};'>{sinal}{format_currency(diferenca)}</b></span>", unsafe_allow_html=True)
                  st.caption("Combinação *hipotética* encontrada pelo algoritmo. Quantidades arredondadas.")
 
                  col1, col2 = st.columns(2) # Mantém as colunas para separar Bebidas de Sanduíches
